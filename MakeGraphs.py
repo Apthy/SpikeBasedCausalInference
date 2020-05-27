a150022d1236fe6128df8c5724b17d4022265cb7
Aaron <37843140+Apthy@users.noreply.github.com>
2020-05-27 15:07:48 +0100

    updated the NpyPrefixes

--- MakeGraphs.py
+++ MakeGraphs.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
-pref = "RDDFashion-28032020 - 0.01 - 32 - 0.9 - 0/"
-fapref = "FAfashion-28032020 - 0.01 - 32 - 0.9 - 0/"
+pref = r"C:\Users\grthy\PycharmProjects\DeepLearning\Output\RDD Data\Long - 0.01 - 1024 - 0.9 - 0+(5)"
+fapref = r"C:\Users\grthy\PycharmProjects\DeepLearning\Output\Functional_Alignment\FAfashion-28032020 - 0.01 - 32 - 0.9 - 0"
 
-correct_1= np.load(pref + "correct_1.npy")
-correct_2= np.load(pref + "correct_2.npy")
-correct_3= np.load(pref + "correct_3.npy")
+correct_1= np.load(pref + "\correct_1.npy")
+correct_2= np.load(pref + "\correct_2.npy")
+correct_3= np.load(pref + "\correct_3.npy")
 #plt.title("Correct"+pref)
 cor1, cor2, cor3 = [],[],[]
 length = int(len(correct_1)/10)
@@ -16,9 +16,9 @@ def drawSignAlignment():
         cor2.append(correct_2[10*i])
         cor3.append(correct_3[10*i])
 
-    facorrect_1= np.load(fapref + "correct_1.npy")
-    facorrect_2= np.load(fapref + "correct_2.npy")
-    facorrect_3= np.load(fapref + "correct_3.npy")
+    facorrect_1= np.load(fapref + "\correct_1.npy")
+    facorrect_2= np.load(fapref + "\correct_2.npy")
+    facorrect_3= np.load(fapref + "\correct_3.npy")
     plt.title("FC1")
     plt.ylim(0,100)
     plt.xlim(0,length)
@@ -44,21 +44,21 @@ def drawSignAlignment():
 #plt.legend(handles=[Layer1, Layer2, Layer3])
 #plt.show()
 def drawWeights():
-    weight_1= np.load(pref + "weight_1.npy")
-    weight_2= np.load(pref + "weight_2.npy")
-    weight_3= np.load(pref + "weight_3.npy")
+    weight_1= np.load(pref + "\\weight_1.npy")
+    weight_2= np.load(pref + "\\weight_2.npy")
+    weight_3= np.load(pref + "\\weight_3.npy")
 
-    faweight_1= np.load(fapref + "weight_1.npy")
-    faweight_2= np.load(fapref + "weight_2.npy")
-    faweight_3= np.load(fapref + "weight_3.npy")
+    faweight_1= np.load(fapref + "\\weight_1.npy")
+    faweight_2= np.load(fapref + "\\weight_2.npy")
+    faweight_3= np.load(fapref + "\\weight_3.npy")
 
-    fb_weight_1= np.load(pref + "fb_weight_1.npy").T
-    fb_weight_2= np.load(pref + "fb_weight_2.npy").T
-    fb_weight_3= np.load(pref + "fb_weight_3.npy").T
+    fb_weight_1= np.load(pref + "\\fb_weight_1.npy").T
+    fb_weight_2= np.load(pref + "\\fb_weight_2.npy").T
+    fb_weight_3= np.load(pref + "\\fb_weight_3.npy").T
 
-    fafb_weight_1= np.load(fapref + "fb_weight_1.npy").T
-    fafb_weight_2= np.load(fapref + "fb_weight_2.npy").T
-    fafb_weight_3= np.load(fapref + "fb_weight_3.npy").T
+    fafb_weight_1= np.load(fapref + "\\fb_weight_1.npy").T
+    fafb_weight_2= np.load(fapref + "\\fb_weight_2.npy").T
+    fafb_weight_3= np.load(fapref + "\\fb_weight_3.npy").T
 
     flatweight_1=weight_1.flatten()
     flatweight_2=weight_2.flatten()
@@ -131,13 +131,13 @@ def drawWeights():
 
 
 def drawSelf():
-    faself_1 = np.load(fapref + "self_1.npy")
-    faself_2= np.load(fapref + "self_2.npy")
-    faself_3 = np.load(fapref + "self_3.npy")
+    faself_1 = np.load(fapref + "\\self_1.npy")
+    faself_2= np.load(fapref + "\\self_2.npy")
+    faself_3 = np.load(fapref + "\\self_3.npy")
 
-    self_1 = np.load(pref + "self_1.npy")
-    self_2= np.load(pref + "self_2.npy")
-    self_3 = np.load(pref + "self_3.npy")
+    self_1 = np.load(pref + "\\self_1.npy")
+    self_2= np.load(pref + "\\self_2.npy")
+    self_3 = np.load(pref + "\\self_3.npy")
 
     tself1 = []
     tself2 = []
@@ -183,9 +183,9 @@ def drawSelf():
 #plt.legend([Layer1, Layer2,Layer3])
 #plt.show()
 def printInfo():
-    sym1= np.load(pref + "correct_1.npy")
-    sym2= np.load(pref + "correct_2.npy")
-    sym3= np.load(pref + "correct_3.npy")
+    sym1= np.load(pref + "\\correct_1.npy")
+    sym2= np.load(pref + "\\correct_2.npy")
+    sym3= np.load(pref + "\\correct_3.npy")
     sim1 = []
     sim2 = []
     sim3 = []
@@ -203,8 +203,8 @@ def printInfo():
     plt.show()
 
 def drawErr():
-    RDDerr = np.load(pref + "train_err.npy")
-    faerr = np.load(fapref + "train_err.npy")
+    RDDerr = np.load(pref + "\\train_err.npy")
+    faerr = np.load(fapref + "\\train_err.npy")
 
     plt.title("error "+ pref)
     Layer1,=plt.plot(RDDerr[1:101],color="m")
@@ -213,8 +213,8 @@ def drawErr():
     #plt.legend(handles=[Layer1, Layer2,Layer3])
     plt.show()
 
-#drawSignAlignment()
-#drawWeights()
-#drawSelf()
-#printInfo()
+drawSignAlignment()
+drawWeights()
+drawSelf()
+printInfo()
 drawErr()
